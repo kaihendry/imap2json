d43a57b78e22e1f15443b9494716005b3ecee5bd
Kai Hendry <hendry@webconverger.com>
2014-02-05 17:43:27 +0800

    Add some timing code

--- html/main.js
+++ html/main.js
@@ -2,27 +2,35 @@ var mailjson = {};
 
 function threadview(id) {
 	// console.log(id);
+	console.time("Finding thread");
 	var arg = id.split('-'); // hash of first message - UID
 	id = arg[0];
 	//console.log(id);
-	if (! parseInt(arg[1]) > 0) { window.scrollTo(0,0);}
-	for (i=0; i<mailjson.length; i++) {
+	if (!parseInt(arg[1]) > 0) {
+		window.scrollTo(0, 0);
+	}
+	for (i = 0; i < mailjson.length; i++) {
 		// console.log(id);
 		if (mailjson[i].Id.indexOf(id) == 0) {
 			break;
 		}
 	}
+	console.timeEnd('Finding thread');
 
 	// console.log(i);
-	if (typeof mailjson[i] === 'undefined') { $('#title').html("Thread not found."); return; }
+	if (typeof mailjson[i] === 'undefined') {
+		$('#title').html("Thread not found.");
+		return;
+	}
 
 	// console.log("Found ", id, " at ", i);
-
 	$('#title').html(mailjson[i].Msgs[0].Header.Subject);
+
+	console.time('Rendering thread');
 
 	$.each(mailjson[i].Msgs, function(index, value) {
 		msg = "<div class=mail>";
-		msg += '<a title="UID" id=' + id + '-' +  value.UID + ' class="uid">' + value.UID + '</a>'
+		msg += '<a title="UID" id=' + id + '-' + value.UID + ' class="uid">' + value.UID + '</a>'
 		msg += '<span class="from">';
 		msg += '<span class="name"><span>' + value.Header.From + '</span>'
 		msg += '</span>';
@@ -32,8 +40,9 @@ function threadview(id) {
 		msg += '<time class="time">' + value.Header.Date + '</time>';
 		msg += "<hr><pre>";
 		msg += $("<pre/>").text(value.Body).html();
-        msg += "</pre></div>";
+		msg += "</pre></div>";
 		$("#conversation").append(msg);
+		console.timeEnd('Rendering thread');
 		// console.log(index + ": " + value);
 	});
 }
@@ -43,15 +52,20 @@ function main() {
 	if (id) {
 		threadview(id);
 	} else {
+		console.time('Rendering index');
 		$("#title").html('mail2json Index');
 		$.each(mailjson, function(index, value) {
 			// console.log(value.Id)
 			try {
-			$("#conversation").append("<li><strong><a href=#" + value.Id + ">" + value.Id + "</strong> " + value.Msgs.length + " Subject: " + value.Msgs[0].Header.Subject + "</a></li>");
-			} catch (e) {
+				var c = "<li><a href=#" ;
+				c += value.Id + "><span class='threadlength'>" + value.Msgs.length;
+				c += "</span> <time>" + value.Msgs[0].Header.Date + "</time>&nbsp;<strong>" +value.Msgs[0].Header.Subject + "</strong></a></li>";
+				$("#conversation").append(c);
+			} catch(e) {
 				console.log(value, e);
 			}
 		});
+		console.timeEnd('Rendering index');
 	}
 }
 
